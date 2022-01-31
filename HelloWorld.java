0705a3abffb8f05048cd1eb1f9cf854fba26d1fc
Malgosiatobiasz1 <37155820+Malgosiatobiasz1@users.noreply.github.com>
2022-01-31 19:38:44 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
   public static void main(String[] args) {
-        System.out.println("Hello, World edted by me!");
+        System.out.println("Hello, World edted by me! webhook test");
     }
 }
